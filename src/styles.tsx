ad6a5e95478adbba01c18344f3eb6e8fc5af6563
Gabriel Fernandes <gabriel.fernandes@localiza.com>
2022-12-19 17:17:52 -0300

    feat: setup project

---
+++ src/styles.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material';
+import { styled } from '@mui/material';
+
+export const Centered = styled(Box)`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  margin-top: -50px;
+  margin-left: -100px;
+`;
